506dcd740fdd73f7e63a6079560d8a2c37f38259
pkusensei <ZW_08@outlook.com>
2018-10-27 21:11:27 -0400

    parser not done

---
+++ MCF/Parser.cpp
@@ -0,0 +1,79 @@
+#include "stdafx.h"
+#include "Parser.h"
+#include "SyntaxNode.h"
+#include "Diagnostic.h"
+
+namespace MCF {
+
+Parser::Parser(std::string text)
+{
+	_tokens = std::vector<SyntaxToken>();
+	Lexer lexer(text);
+	std::unique_ptr<SyntaxToken> pToken;
+	do
+	{
+		pToken = std::make_unique<SyntaxToken>(lexer.Lex());
+		if (pToken->Kind() != SyntaxKind::WhitespaceToken &&
+			pToken->Kind() != SyntaxKind::BadToken)
+			_tokens.emplace_back(*pToken);
+	} while (pToken != nullptr &&
+			 pToken->Kind() != SyntaxKind::EndOfFileToken);
+	_diagnostics->AddRange(*lexer.Diagnostics());
+}
+
+Parser::~Parser()
+{
+}
+
+SyntaxToken Parser::Peek(int offset) const
+{
+	auto idx = _position + offset;
+	if (idx >= _tokens.size())
+		return *_tokens.end();
+	return _tokens[idx];
+}
+
+SyntaxToken Parser::Current() const
+{
+	return Peek(0);
+}
+
+SyntaxToken Parser::NextToken()
+{
+	auto current = Current();
+	_position++;
+	return current;
+}
+
+SyntaxToken Parser::MatchToken(SyntaxKind kind)
+{
+	auto current = Current();
+	if (current.Kind() == kind)
+		return NextToken();
+	_diagnostics->ReportUnexpectedToken(current.Span(), current.Kind(), kind);
+	return SyntaxToken(kind, current.Position(), "", NULL);
+}
+
+SyntaxTree Parser::Parse()
+{
+	// TODO
+	//auto expression = std::make_unique<ExpressionSyntax>(ParseExpression());
+	auto endOfFileToken = MatchToken(SyntaxKind::EndOfFileToken);
+	return SyntaxTree(_diagnostics, expression, endOfFileToken);
+}
+
+SyntaxTree::SyntaxTree(std::unique_ptr<DiagnosticBag>& diagnostics, std::unique_ptr<ExpressionSyntax>& root, 
+					   SyntaxToken& endOfFileToken)
+{
+	_root.swap(root);
+	_diagnostics.swap(diagnostics);
+	_endOfFileToken = std::make_unique<SyntaxToken>(endOfFileToken);
+}
+
+SyntaxTree SyntaxTree::Parse(const std::string & text)
+{
+	Parser parser(text);
+	return parser.Parse();
+}
+
+}//MCF
---
+++ MCF/Parser.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include "common.h"
+
+namespace MCF {
+
+class SyntaxNode;
+class SyntaxToken;
+class DiagnosticBag;
+enum class SyntaxKind;
+class ExpressionSyntax;
+
+class SyntaxTree;
+
+class Parser final
+{
+private:
+	std::vector<SyntaxToken> _tokens;
+	int _position;
+	std::unique_ptr<DiagnosticBag> _diagnostics;
+
+	SyntaxToken Peek(int offset) const;
+	SyntaxToken Current() const;
+	SyntaxToken NextToken();
+	SyntaxToken MatchToken(SyntaxKind kind);
+
+	/*ExpressionSyntax ParseExpression() const;
+	ExpressionSyntax ParseAssignmentExpression() const;
+	ExpressionSyntax ParseBinaryExpression(int parentPrecedence = 0) const;
+	ExpressionSyntax ParsePrimaryExpression() const;*/
+public:
+	MCF_API Parser(std::string text);
+	MCF_API ~Parser();
+
+	DiagnosticBag* Diagnostics()const { return _diagnostics.get(); }
+	SyntaxTree Parse();
+};
+
+class SyntaxTree
+{
+private:
+	std::unique_ptr<DiagnosticBag> _diagnostics;
+	std::unique_ptr<ExpressionSyntax> _root;
+	std::unique_ptr<SyntaxToken> _endOfFileToken;
+public:
+	SyntaxTree(std::unique_ptr<DiagnosticBag>& diagnostics, std::unique_ptr<ExpressionSyntax>& root,
+			   SyntaxToken& endOfFileToken);
+
+	static SyntaxTree Parse(const std::string& text);
+};
+
+}//MCF
