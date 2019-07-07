347cbaebd8077740ea1323071a72481e4dbd5a6c
pkusensei <ZW_08@outlook.com>
2019-07-06 21:06:00 -0400

    proper use of GENERATE in catch2

--- ctest/src/SourceText_tests.cpp
+++ ctest/src/SourceText_tests.cpp
@@ -4,15 +4,18 @@
 
 TEST_CASE("SourceText includes last line", "[SourceText]")
 {
-	auto data = std::vector<std::pair<std::string, size_t>>{
-		{".", 1},
-		{"\r\n", 2},
-		{".\r\n\r\n", 3},
-	};
-	for (const auto& it : data)
+	auto [input, expected] = GENERATE(
+		// or values<std::pair<std::string, size_t>>
+		table<std::string, size_t>({
+			{".", 1},
+			{"\r\n", 2},
+			{".\r\n\r\n", 3},
+			}));
+
+	SECTION("One pair")
 	{
-		auto sourceText = MCF::SourceText::From(it.first);
-		auto& lines = sourceText.Lines();
-		REQUIRE(it.second == lines.size());
+		auto text = MCF::SourceText::From(input);
+		auto& lines = text.Lines();
+		CHECK(lines.size() == expected);
 	}
 }
