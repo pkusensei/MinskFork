1dcfb6305c62fe0afb27b0e96e94990305453314
pkusensei <ZW_08@outlook.com>
2020-03-26 15:17:17 -0400

    compiler supports single file

--- mcfc/src/mcfc.cpp
+++ mcfc/src/mcfc.cpp
@@ -1,6 +1,43 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
-int main()
+#include "Compilation.h"
+#include "Diagnostic.h"
+#include "IO.h"
+#include "Parsing.h"
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello world!";
+	if (argc == 1)
+	{
+		std::cerr << "usage: mcfc <source-paths>" << '\n';
+		return 0;
+	}
+	if (argc > 2)
+	{
+		std::cout << "error: only one path supported right now" << '\n';
+		return 0;
+	}
+
+	auto path = argv[1];
+	auto file = std::ifstream(path);
+	auto ss = std::stringstream();
+	ss << file.rdbuf();
+
+	auto tree = MCF::SyntaxTree::Parse(ss.str());
+	auto compilation = MCF::Compilation(tree);
+	MCF::VarMap variables;
+	auto result = compilation.Evaluate(variables);
+
+	if (result.Diagnostics()->empty())
+	{
+		if (result.Value().HasValue())
+			std::cout << result.Value() << '\n';
+	} else
+	{
+		auto writer = MCF::IndentedTextWriter(std::cerr);
+		writer.WriteDiagnostics(*result.Diagnostics(), *compilation.Syntax());
+	}
+
 }
